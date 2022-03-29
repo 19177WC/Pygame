61e903f1d8f1cec0ce2bbf54a7f9ee794f9fb6f1
19177Wc <19177@wc.school.nz>
2022-03-30 09:56:45 +1300

    Making rectangle

--- python game.py
+++ python game.py
@@ -13,12 +13,15 @@ bg = pygame.image.load('bg.jpg')
 clock = pygame.time.Clock()
 
 
+colour = (255, 0 , 0)
+
+pygame.display.flip()
 #---------------------------- Methods ----------------------------
 def redraw_GameWindow():
     window.blit(bg, (0,0)) # This will draw our background image at (0,0) .blit is short for block
     # transfer and allows us to add images to a surface, in this case the main window.
+    pygame.draw.rect(window, colour, pygame.Rect(30, 30, 60, 60))
     pygame.display.update()
-
 #---------------------------- Main Routine ----------------------------
 run=True
 
