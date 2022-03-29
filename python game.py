a5914dbbdb5225fa6bb9ded89fe2456c04799923
19177Wc <19177@wc.school.nz>
2022-03-30 10:16:59 +1300

    Random position - next make rectangle move

--- python game.py
+++ python game.py
@@ -1,12 +1,17 @@
 import pygame
+import random
+from random import randint
 from pygame import time
 pygame.init()
-window = pygame.display.set_mode((800, 490))
+window = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("Game Window")
 x = 500
 y = 400
 width = 100
 height = 100
+xRange = random.randint(0, 800)
+yRange = random.randint(0, 500)
+yMovement = 500
 
 bg = pygame.image.load('bg.jpg')
 
@@ -20,7 +25,7 @@ pygame.display.flip()
 def redraw_GameWindow():
     window.blit(bg, (0,0)) # This will draw our background image at (0,0) .blit is short for block
     # transfer and allows us to add images to a surface, in this case the main window.
-    pygame.draw.rect(window, colour, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.rect(window, colour, pygame.Rect(xRange, 500, 60, 60))
     pygame.display.update()
 #---------------------------- Main Routine ----------------------------
 run=True
