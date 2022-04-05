227586a3947948dacffe74824f5e88d1a8e58456
19177WC <19177@wc.school.nz>
2022-04-06 10:16:12 +1200

    eiwfgwiuefviwe

--- python game.py
+++ python game.py
@@ -9,7 +9,7 @@ x = 500
 y = 0
 width = 100
 height = 100
-xRange = random.randint(0, 800)
+xRange = random.randint(50, 750)
 yMovement = random.randint(5, 15)
 RedColour = random.randint(0, 255)
 GreenColour = random.randint(0, 255)
@@ -18,7 +18,7 @@ colour = (RedColour, GreenColour , BlueColour)
 bg = pygame.image.load('bg.jpg')
 clock = pygame.time.Clock()
 Rect = pygame.draw.rect(window, colour, (xRange, y, 50, 50))
-
+Score = 0
 
 
 pygame.display.flip()
@@ -40,13 +40,20 @@ while run:
             run = False
 
         elif ( event.type == pygame.MOUSEBUTTONUP ):
-            mouse_position = pygame.mouse.get_pos()             # Location of the mouse-click
-            if ( Rect.collidepoint( mouse_position ) ):   # Was that click inside our rectangle
+            mouse_position = pygame.mouse.get_pos()             # Location of the mouse click
+            if ( Rect.collidepoint( mouse_position ) ):   # Was that click inside our object
                 print( "hit" )
+                y = 0
+                xRange = random.randint(50, 750)
+                yMovement = random.randint(5, 15)
+                RedColour = random.randint(0, 255)
+                GreenColour = random.randint(0, 255)
+                BlueColour = random.randint(0, 255)
+                colour = (RedColour, GreenColour, BlueColour)
 
     if y > 500:
         y = 0
-        xRange = random.randint(0, 800)
+        xRange = random.randint(50, 750)
         yMovement = random.randint(5, 15)
         RedColour = random.randint(0, 255)
         GreenColour = random.randint(0, 255)
