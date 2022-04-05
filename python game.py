0fde2b2e0342a6c6b7afca01b45c7dc2114011d4
19177Wc <19177@wc.school.nz>
2022-04-05 12:31:58 +1200

    iwebufliebigre

--- python game.py
+++ python game.py
@@ -6,7 +6,7 @@ pygame.init()
 window = pygame.display.set_mode((800, 480))
 pygame.display.set_caption("Game Window")
 x = 500
-y = 500
+y = 0
 width = 100
 height = 100
 xRange = random.randint(0, 800)
@@ -16,31 +16,36 @@ GreenColour = random.randint(0, 255)
 BlueColour = random.randint(0, 255)
 colour = (RedColour, GreenColour , BlueColour)
 bg = pygame.image.load('bg.jpg')
-
 clock = pygame.time.Clock()
-
+Rect = pygame.draw.rect(window, colour, (xRange, y, 50, 50))
 
 
 
 pygame.display.flip()
 #---------------------------- Methods ----------------------------
 def redraw_GameWindow():
+    global Rect
     window.blit(bg, (0,0)) # This will draw our background image at (0,0) .blit is short for block
     # transfer and allows us to add images to a surface, in this case the main window.
-    pygame.draw.circle(window, colour, [xRange, y], 50, 0)
+    Rect = pygame.draw.rect(window, colour, (xRange, y, 50, 50))
     pygame.display.update()
 #---------------------------- Main Routine ----------------------------
 run=True
 
 while run:
     clock.tick(27)
-    y -= yMovement
+    y += yMovement
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    if y < 0:
-        y = 500
+        elif ( event.type == pygame.MOUSEBUTTONUP ):
+            mouse_position = pygame.mouse.get_pos()             # Location of the mouse-click
+            if ( Rect.collidepoint( mouse_position ) ):   # Was that click inside our rectangle
+                print( "hit" )
+
+    if y > 500:
+        y = 0
         xRange = random.randint(0, 800)
         yMovement = random.randint(5, 15)
         RedColour = random.randint(0, 255)
